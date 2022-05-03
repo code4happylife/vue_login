57c61a0ed77d76b9a125653161df61a9ffa3e487
donngchao <1559273720@qq.com>
2022-05-04 07:02:36 +0800

    vue login

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <LoginBox></LoginBox>
+</template>
+
+<script>
+import LoginBox from "@/components/LoginBox";
+
+export default {
+  name: 'App',
+  components: {
+    LoginBox
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LoginBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>登录页面</h1>
+  <div class="login_box">
+    <el-form :model="form" label-width="120px" style="width: 100%">
+      <el-form-item label="用户名">
+        <el-input v-model="form.name"/>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.pwd"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginBox",
+  data() {
+    return {
+      form: {
+        name: "",
+        pwd: ""
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      alert('login success!')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login_box {
+padding: 0px 30px ;
+}
+</style>
